6e07a7f0653f81fd408d283fa2861e456bd48198
Sylvie-Saidi <sylvie.saidi22@gmail.com>
2020-11-01 19:00:09 +0100

    organisation dossier quizz + ajout elements reminder

---
+++ src/components/ElementsReminder/ElementsReminder.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import "./ElementsReminder.css";
+
+function ElementsReminder() {
+  return (
+    <div className="ElementsReminderContainer">
+      <img
+        id="Cleverness"
+        className="ElementsIcons"
+        src="https://www.uzunetap.com/files/uploads/logo-placeholder@2x.png"
+        alt="Cleverness icon"
+      />
+<i class="fas fa-long-arrow-alt-right"></i>
+      <img
+        id="Resistance"
+        className="ElementsIcons"
+        src="https://www.uzunetap.com/files/uploads/logo-placeholder@2x.png"
+        alt="Resistance icon"
+      />
+      <i class="fas fa-long-arrow-alt-down"></i>
+      <img
+        id="Speed"
+        className="ElementsIcons"
+        src="https://www.uzunetap.com/files/uploads/logo-placeholder@2x.png"
+        alt="Speed icon"
+      />
+<i class="fas fa-long-arrow-alt-left"></i>
+      <img
+        id="Strength"
+        className="ElementsIcons"
+        src="https://www.uzunetap.com/files/uploads/logo-placeholder@2x.png"
+        alt="Strength icon"
+      />
+      <i class="fas fa-long-arrow-alt-up"></i>
+    </div>
+  );
+}
+export default ElementsReminder;
