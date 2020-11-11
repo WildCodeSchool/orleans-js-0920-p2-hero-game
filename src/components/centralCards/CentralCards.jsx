9766cc031816addea7e3c0035a56146bd5fed6ff
MartyLucas45 <marty45@hotmail.fr>
2020-11-11 17:18:47 +0100

    Recup API dans tableaux

--- src/components/centralCards/CentralCards.jsx
+++ src/components/centralCards/CentralCards.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useEffect, useState } from 'react'
 import './centralCard.css'
 import Anim from './AnimAttack'
 import Superman from '../cards/Carte_Superman-min.png'
@@ -7,40 +7,47 @@ import Mera from '../cards/Carte_Mera-min.png'
 import CardChoice from './CardChoice'
 import CardLeave from './CardLeave'
 
-class CentralCard extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            enemyCard: Superman,
-            userCard: Mera,
-        };
-    }
+function CentralCard() {
 
-    render() {
+    const [fullChamp, setFullchamp] = useState([])
+
+    const [userHero, setUserHero] = useState(Mera)
+    const [enemyHero, setEnemeyEHero] = useState(Superman)
+
+    const [btnInt, setbtnInt] = useState(0)
+    const [btnStr, setbtnStr] = useState(0)
+    const [btnSpe, setbtnSpe] = useState(0)
+    const [btnRes, setbtnRes] = useState(0)
+
+    useEffect(() => {
+        fetch('https://my-super-heroes-api-wcs.herokuapp.com/heroes')
+        .then(res => res.json())
+        .then(data => setFullchamp([data]));
+        console.log(fullChamp)
+      }, []);
         
-        return(
-            
-            <div >
-                <div className="CentralDiv">    
-                <div className="DivUser" onMouseEnter={CardChoice} onMouseLeave={CardLeave}>
-                    {/* <p className="TitleCard">JOKER</p> */}
-                    <picture className="Card"><img className="UserCard" src={this.state.userCard} alt="Carte User en jeu" /></picture>
-                    <button className="ButtonAttack1 BtnU" onClick={Anim}></button>
-                    <button className="ButtonAttack2 BtnU" onClick={Anim}></button>
-                    <button className="ButtonAttack3 BtnU" onClick={Anim}></button>
-                    <button className="ButtonAttack4 BtnU" onClick={Anim}></button>
-                </div>
-                
-                <div>
-                    {/* <p className="TitleCard">SUPERMAN</p> */}
-                    <picture className="Card"><img className="EnemyCard" src={this.state.enemyCard} alt="Carte Enemy en jeu"/></picture>
-                </div>
-                </div>
-            </div>
-
-        )
     
-    }
+
+    return(
+            
+        <div >
+            <div className="CentralDiv">    
+            <div className="DivUser" onMouseEnter={CardChoice} onMouseLeave={CardLeave}>
+                <picture className="Card"><img className="UserCard" src={userHero} alt="Carte User en jeu" /></picture>
+                <button className="ButtonAttack1 BtnU" onClick={Anim}></button>
+                <button className="ButtonAttack2 BtnU" onClick={Anim}></button>
+                <button className="ButtonAttack3 BtnU" onClick={Anim}></button>
+                <button className="ButtonAttack4 BtnU" onClick={Anim}></button>
+            </div>
+                
+            <div>
+                <picture className="Card"><img className="EnemyCard" src={enemyHero} alt="Carte Enemy en jeu"/></picture>
+            </div>
+            </div>
+        </div>
+
+    )
+    
 };
 
 
