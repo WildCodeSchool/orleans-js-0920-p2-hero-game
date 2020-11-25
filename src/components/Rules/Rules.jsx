79608f17d3d968ec72ffeac164b203c83fa52f5e
Sylvie-Saidi <sylvie.saidi22@gmail.com>
2020-11-25 09:35:43 +0100

    changes requested done

--- src/components/Rules/Rules.jsx
+++ src/components/Rules/Rules.jsx
@@ -6,21 +6,21 @@ export default function Rules () {
         <div className="rules-container">
             <h2 className="main-rules-title">Règles du jeu</h2>
                 <p className="rules-intro">
-                     Le but du jeu est très simple : éliminer le héro adverse en lui infligeant des dégâts, avant qu’il ne vous élimine ! Attention, il contre-attaque !
+                     Le but du jeu est très simple : éliminer le héros adverse en lui infligeant des dégâts, avant qu’il ne vous élimine ! Attention, il contre-attaque !
                 </p>
                        <div className="rules-cards">
                          <div className="div-rules">
-                             <h3 className="rules-title">Choisir un héro</h3>
-                                 <p>Commencez par sélectionner votre super héro dans le menu de sélection. Choisissez-le bien ! Ses caractéristiques sont présentées sur la carte, vous y trouverez son type dominant et les dégâts infligés par ses attaques. Chaque héro possède une attaque de chaque type ! La plus puissante étant généralement celle de son type de prédilection. </p>
+                             <h3 className="rules-title">Choisir un héros</h3>
+                                 <p>Commencez par sélectionner votre super héros dans le menu de sélection. Choisissez-le bien ! Ses caractéristiques sont présentées sur la carte, vous y trouverez son type dominant et les dégâts infligés par ses attaques. Chaque héros possède une attaque de chaque type ! La plus puissante étant généralement celle de son type de prédilection. </p>
                         </div>
                         <div className="div-rules">
                              <h3 className="rules-title">Attaquer</h3>
-                                 <p>Pour infliger des dégâts au héro adverse, cliquez tout simplement sur le sort d’attaque désiré situé sur la carte de votre héro. Il peut être judicieux de choisir les sorts efficaces contre le type  du héro adverse, pour lui infliger davantage de dégâts. Vous pouvez utiliser une attaque par tour de jeu. Une fois cette attaque lancée, le héros adverse contre-attaque !</p>
+                                 <p>Pour infliger des dégâts au héros adverse, cliquez tout simplement sur le sort d’attaque désiré situé sur la carte de votre héros. Il peut être judicieux de choisir les sorts efficaces contre le type  du héros adverse, pour lui infliger davantage de dégâts. Vous pouvez utiliser une attaque par tour de jeu. Une fois cette attaque lancée, le héross adverse contre-attaque !</p>
                          </div>
                          <div className="div-rules">
                             <h3 className="rules-title">Fin du Combat</h3>
                             <p>
-                            Le combat se termine lorsque l’un des super héros en jeu n’a plus de points de vie. Vous pouvez savourer votre victoire ou appuyer sur le bouton “Rejouer” pour relancer une partie ! 
+                            Le combat se termine lorsque l’un des super héross en jeu n’a plus de points de vie. Vous pouvez savourer votre victoire ou appuyer sur le bouton “Rejouer” pour relancer une partie ! 
                             </p>
                         </div>
                         </div>
@@ -29,7 +29,7 @@ export default function Rules () {
                             <h3 className="rules-title-reminder">Rappel des éléments</h3>
                             <div className="div-p1">
                             <p className="p1">
-                            Chaque héro a un type d'élément de prédilection bien défini. Il existe 4 types d’éléments : 
+                            Chaque héros a un type d'élément de prédilection bien défini. Il existe 4 types d’éléments : 
                             </p>
                             <ul>
                             <li>Force</li>
