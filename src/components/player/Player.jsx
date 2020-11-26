206032a00314925cb0211da983ee1f094b5abdca
MartyLucas45 <marty45@hotmail.fr>
2020-11-26 14:14:04 +0100

    26/11 Fix Barre noires vidéo

--- src/components/player/Player.jsx
+++ src/components/player/Player.jsx
@@ -5,13 +5,22 @@ import { Switch, Route, Link } from 'react-router-dom';
 
 function Player() {
     return(
-        <div>
+        <div className="PlayerTotal">
             <div className="PlayerMain" id="home">
-            <Link className="buttonplay" to="/choosehero" ><h2 className="play">JOUER</h2></Link>
-               <video className="video"
-                autoPlay
-                loop
-                muted>
+                <div className="Color1"></div>
+                <div className="DivTitle">
+                    <h1 className="Title">MY SUP'R HERO</h1>
+                    {/* <div className="DivButton">
+                        <h2 className="SecondNavTitle">Les Personnages</h2>
+                        <h2 className="SecondNavTitle">Le Quizz</h2>
+                    </div> */}
+                </div>
+                <div className="Color2"></div>
+                <Link className="buttonplay" to="/choosehero" ><h2 className="play">JOUER</h2></Link>
+                <video className="video"
+                    autoPlay
+                    loop
+                    muted>
                     <source src= {Videotraileravenger}
                     type="video/mp4"/>
                </video>
